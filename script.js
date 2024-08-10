6e7899665e0581165d00cdd609b806da75982338
roki <151161348+rokidory@users.noreply.github.com>
2024-08-10 13:23:27 +0700

    added functionality to color mode buttons

---
+++ script.js
@@ -0,0 +1,51 @@
+const lightModeBtn = document.getElementById("light-mode");
+const darkModeBtn = document.getElementById("dark-mode");
+const hyperModeBtn = document.getElementById("hyper-mode");
+const html = document.querySelector("html");
+
+function changeColorMode(mode) {
+    if (mode === 'light') {
+        html.style.setProperty('--background', "#c1e1d3");
+        html.style.setProperty('--light', "#89c59a");
+        html.style.setProperty('--accent', "#0fa6d0");
+        html.style.setProperty('--text1', "#328f73");
+        html.style.setProperty('--text2', "black");
+        html.style.setProperty('--top1', "#bbfff5");
+        html.style.setProperty('--top2', "#26a390");
+        html.style.setProperty('--border', "black");
+        html.style.setProperty('--hover', "#00000061");
+        html.style.backgroundImage = "url('https://i.ibb.co/M7WhPv9/Screenshot-2024-08-07-130530.png')";
+    } else if (mode === 'dark') {
+        html.style.setProperty('--background', "#032922");
+        html.style.setProperty('--light', "#063b31");
+        html.style.setProperty('--accent', "#49875b");
+        html.style.setProperty('--text1', "#afe1ed");
+        html.style.setProperty('--text2', "#c1e1d3");
+        html.style.setProperty('--top1', "#0dd6a6");
+        html.style.setProperty('--top2', "#0f735b");
+        html.style.setProperty('--border', "#ffffff");
+        html.style.setProperty('--hover', "#ffffff61");
+        html.style.backgroundImage = "url('https://i.ibb.co/YTbN05R/Screenshot-2024-08-07-130603.png')";
+    } else { // hypersaturated
+        html.style.setProperty('--background', "#00ff99");
+        html.style.setProperty('--light', "#ff4040");
+        html.style.setProperty('--accent', "#ff3b3b");
+        html.style.setProperty('--text1', "#0f00de");
+        html.style.setProperty('--text2', "#0f00de");
+        html.style.setProperty('--top1', "#ff009d");
+        html.style.setProperty('--top2', "#c300ff");
+        html.style.setProperty('--border', "#ddff00");
+        html.style.setProperty('--hover', "#ff770061");
+        html.style.backgroundImage = "url('https://i.ibb.co/6N0fvYd/Screenshot-2024-08-07-130745.png')";
+    }
+}
+
+lightModeBtn.addEventListener("click", () => {
+    changeColorMode('light')
+});
+darkModeBtn.addEventListener("click", () => {
+    changeColorMode('dark')
+});
+hyperModeBtn.addEventListener("click", () => {
+    changeColorMode('hyper')
+});
